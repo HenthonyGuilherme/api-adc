bc2c2521809ed9f902306f540cd53ec373d367e4
HenthonyGuilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-11-06 21:00:06 -0300

    novo app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 
 require('dotenv').config()
 
-const  cors = require('cors')
+const cors = require('cors')
 
 const PORT = process.env.PORT
 
@@ -14,58 +14,51 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/hello_world', (req, res) => {
-    res.send('aprendendo api')
+    res.send('hello world')
 })
 
 app.post('/create', (req, res) => {
     const title = req.body.title
 
-    res.send(`Titulo: ${title}`)
+    res.send(`Post: ${title}`)
 })
 
-// Criar um poste (objeto)
 app.post('/create_post', async (req, res) => {
-
     try {
         const { title, content } = req.body
-    
+        
         const post = await Post.create({ title, content })
-
+        
         res.send(post)
-    }catch(err){
-        res.status(400).send(err)
-    }   
-})
-
-// Lista os postes enviados 
-app.get('/list_posts', async (req, res) => {
-    try {
-        const posts = await Post.find()
-
-        res.send({ posts })
-    }catch(err){
-        res.status(400).send(err)
-    } 
-})
-
-// Pega so um parametro pela URL
-app.get('/show_post/:post_id', async (req, res) =>{
-    try {
-
-        const postId = req.params.post_id
-
-        const post = await Post.findById(postId)
-        // filtro no objeto | find({ _id: postId }) | podemos coloca mas cosnt
-
-        res.send({ post })
     }catch(err) {
         res.status(400).send(err)
     }
 })
 
-// Atualizar o objeto | put : atualiza tudo | patch: atualiza parcialmente
-app.patch('/update_post/:post_id', async (req, res) =>{
-    try{
+app.get('/list_posts', async (req, res) => {
+    try {
+        const posts = await Post.find()
+
+        res.send({ posts })
+    }catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+app.get('/show_post/:post_id', async (req, res) => {
+    try {
+        const postId = req.params.post_id
+
+        const post = await Post.findById(postId)
+
+        res.send({ post })
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+app.patch('/update_post/:post_id', async (req, res) => {
+    try {
         const postId = req.params.post_id
 
         const { title, content } = req.body
@@ -73,21 +66,19 @@ app.patch('/update_post/:post_id', async (req, res) =>{
         const post = await Post.findByIdAndUpdate(postId, { title, content }, { new: true })
 
         res.send({ post })
-
-    } catch(err){
+    } catch(err) {
         res.status(400).send(err)
     }
 })
 
-// Deletar postaguem (objeto)
-app.delete('/deleTe_post/:post_id', async (req, res) => {
-    try{
+app.delete('/delete_post/:post_id', async (req, res) => {
+    try {
         const postId = req.params.post_id
 
         await Post.findByIdAndDelete(postId)
 
         res.send({ msg: 'Deletado com sucesso' })
-    } catch(err){
+    } catch(err) {
         res.status(400).send(err)
     }
 })
